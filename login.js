30d5508092ca13513b653b5527e983017968e69b
mixup-svg <raqeemirfan58@gmail.com>
2024-12-20 16:03:38 +0500

    google sign-in

--- login.js
+++ login.js
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const idToken = response.credential;
 
       // Send the ID token to your backend for verification
-      fetch("http://127.0.0.1:8080/verify-token", {
+      fetch("https://noty-notepad.netlify.app/verify-token", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
